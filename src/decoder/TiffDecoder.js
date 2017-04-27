5501c55fdc6f679f683f09352a526891f4d450e4
Jonathan Lurie <jlurie@ace-ws-50.cbrain.mcgill.ca>
2017-04-27 16:56:48 -0400

    ADD another tiff module

--- src/decoder/TiffDecoder.js
+++ src/decoder/TiffDecoder.js
@@ -1,8 +1,19 @@
+/*
+* Author   Jonathan Lurie - http://me.jonahanlurie.fr
+* License  MIT
+* Link      https://github.com/jonathanlurie/pixpipejs
+* Lab       MCIN - Montreal Neurological Institute
+*/
 
-import tiff from 'tiff';
+//import tiff from 'tiff';
+import { UTIF } from '../non_npm_modules/UTIF.js/UTIF.js';
 import { Filter } from '../core/Filter.js';
 import { Image2D } from '../core/Image2D.js';
 
+
+/**
+* Read and decode Tiff format
+*/
 class TiffDecoder extends Filter {
   constructor() {
     super();
@@ -18,26 +29,50 @@ class TiffDecoder extends Filter {
       return;
     }
     
-    var decoded = tiff.decode( inputBuffer )
+    var utifDecoded = UTIF.decode(inputBuffer);
+    console.log( utifDecoded );
+    
+    if( utifDecoded ){
+      if( utifDecoded.length ){
+        console.log(utifDecoded[0]);
+        var width = utifDecoded[0].width;
+        var height = utifDecoded[0].height;
+        var data = utifDecoded[0].data;
+        var ncpp = data.length / (width * height);
+        
+        var outputImg = this._addOutput( Image2D );
+        outputImg.setData( data, width, height, ncpp);
+        
+      }
+    }
+    
+    /*
+    var decoded = tiff.decode( inputBuffer)
+    
+    console.log(decoded);
     
     if(decoded.length > 0){
       var tiffIfd = decoded[0];
-      /*
+      console.log( tiffIfd.data );
       console.log( tiffIfd.width );
       console.log( tiffIfd.height );
       console.log( tiffIfd.bitsPerSample );
       console.log( tiffIfd.components );
       console.log( tiffIfd.imageDescription );
       console.log( tiffIfd );
-      */
+      
       
       var outputImg = this._addOutput( Image2D );
       outputImg.setData( tiffIfd.data, tiffIfd.width, tiffIfd.height, tiffIfd.components);
-
+      
+      
+      
+      
+      
     }else{
       console.warn("Could no decode this tiff file.");
     }
-    
+    */
     
     
     
