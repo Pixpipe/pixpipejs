16fa603ae97cf3a5ecd79a564ebb8ed30707f07f
Jonathan Lurie <jlurie@ace-ws-50.cbrain.mcgill.ca>
2017-09-13 17:46:22 -0400

    nifti progress

--- src/decoder/NiftiDecoderAlt.js
+++ src/decoder/NiftiDecoderAlt.js
@@ -13,6 +13,34 @@ import { MniVolume } from '../core/MniVolume.js';
 
 
 /**
+* Important information:  
+* NIfTI dataset are using two indexing methods:  
+* - A voxel based system (i, j, k), the most intuitive, where i is the fastest varying dim and k is the sloest varying dim.
+*   Thus for a given (i, j, k) the value is at (i + j*dim[1] + k*dim[1]*dim[2])
+* - A subject based system (x, y, z), where +x is right, +y is anterior, +z is superior (right handed coord system).
+*   This system is CENTER pixel/voxel and is the result of a transformation from (i, j, k) and a scaling given by the size of 
+*   each voxel in a world unit (eg. mm)
+*
+* NIfTI provides three alternatives to characterize this transformation:
+* 
+* METHOD 1 , when header.qform_code = 0  
+* Here, no specific orientation difers in [x, y, z], only spatial scaling based on voxel world dimensions.
+* This method is NOT the default one, neither it is the most common. It is mainly for bacward compatibility
+* to ANALYZE 7.5.
+* Thus we simply have:
+* x = pixdim[1] * i
+* y = pixdim[2] * j
+* z = pixdim[3] * k
+*
+* METHOD 2, the "normal" case, when header.qform_code > 0  
+* In this situation, three components are involved in the transformation:
+* 1. voxel dimensions (header.pixDims[]) for the spatial scaling
+* 2. a rotation matrix, for orientation
+* 3. a shift
+* Thus, we have:
+* [ x ]   [ R11 R12 R13 ] [        header.pixDims[1] * i ]   [ header.qoffset_x ]
+* [ y ] = [ R21 R22 R23 ] [        header.pixDims[2] * j ] + [ header.qoffset_y ]
+* [ z ]   [ R31 R32 R33 ] [ qfac * header.pixDims[3] * k ]   [ header.qoffset_z ]
 * Info:
 * The official NIfTI header description ( https://nifti.nimh.nih.gov/pub/dist/src/niftilib/nifti1.h )
 * was used to interpret the data.
@@ -73,26 +101,96 @@ class NiftiDecoderAlt extends Filter {
     var subjectSpaceNames = ['i', 'j', 'k', 'l'];
     metadata.dimensions = [];
     for(var d=0; d<metadata.numberOfDimensions; d++){
-      var dimensions = {
-        nameVoxelSpace: voxelSpaceNames[d],
-        subjectSpaceName: subjectSpaceNames[d],
-        // TODO
+      // compute the offset based on the previous dim (aka. stride)
+      var offset = 1;
+      for(var pd=0; pd<d; pd++){
+        offset *= header.dims[pd + 1];
       }
       
-      metadata.dimensions.push( dimension )
+      var dimensions = {
+        length: header.dims[d + 1],
+        nameVoxelSpace: voxelSpaceNames[d],
+        subjectSpaceName: subjectSpaceNames[d],
+        voxelSize: header.pixDims[d + 1],
+        offset: offset
+      }
+      
+      metadata.dimensions.push( dimensions )
     }
     
-      {
-        voxelSpaceName: "i",
-        subjectSpaceName: "x",
-        voxelSize: 
-        
-        
-      }
+    // Tranformation method to lookup [x, y, z] when we know [i, j, k], aka. kno subject based from voxel based
+    metadata.niftiTranfoMethod = header.qform_code > 0 ? 2 : 1;
     
+    var extendedMatrix = header.getQformMat();
+    
+    metadata.transformation = {
+      voxelDimensions: header.pixDims.slice(1, header.pixDims.length),
+      voxelDimensionFactors: [1, 1, header.pixDims[0] < 0 ? -1 : 1], // qfac
+      rotationMatrix: [ extendedMatrix[0].slice(0, 3), extendedMatrix[1].slice(0, 3), extendedMatrix[1].slice(0, 3)],
+      offsets: [header.qoffset_x, header.qoffset_y, header.qoffset_z ],
+    }
+    
+    var method1Transformation = function(i, j, k){
+      return {
+        x: metadata.transformation.pixDims[1] * i,
+        y: metadata.transformation.pixDims[2] * j,
+        z: metadata.transformation.pixDims[3] * k
+      }
+    }
+    
+    var voxelToWorld = function(i, j, k){
+      var rotationMatrix = metadata.transformation.rotationMatrix;
+      var voxelDimensions = metadata.transformation.voxelDimensions;
+      var voxelDimensionFactors = metadata.transformation.voxelDimensionFactors;
+      var offsets = metadata.transformation.offsets;
+      
+      var x = rotationMatrix[0][0] * voxelDimensions[0] * i * voxelDimensionFactors[0] + 
+              rotationMatrix[0][1] * voxelDimensions[1] * j * voxelDimensionFactors[1] + 
+              rotationMatrix[0][2] * voxelDimensions[2] * k * voxelDimensionFactors[2] + 
+              offsets[0];
+              
+      var y = rotationMatrix[1][0] * voxelDimensions[0] * i * voxelDimensionFactors[0] + 
+              rotationMatrix[1][1] * voxelDimensions[1] * j * voxelDimensionFactors[1] + 
+              rotationMatrix[1][2] * voxelDimensions[2] * k * voxelDimensionFactors[2] + 
+              offsets[1];
+              
+      var z = rotationMatrix[2][0] * voxelDimensions[0] * i * voxelDimensionFactors[0] + 
+              rotationMatrix[2][1] * voxelDimensions[1] * j * voxelDimensionFactors[1] + 
+              rotationMatrix[2][2] * voxelDimensions[2] * k * voxelDimensionFactors[2] + 
+              offsets[2];
+              
+      return { x: x, y: y, z: z };
+    }
+    
+    var worldToVoxel = function(x, y, z){
+      var rotationMatrix = metadata.transformation.rotationMatrix;
+      var voxelDimensions = metadata.transformation.voxelDimensions;
+      var voxelDimensionFactors = metadata.transformation.voxelDimensionFactors;
+      var offsets = metadata.transformation.offsets;
+      /*
+      var i = rotationMatrix[0][0] * voxelDimensions[0] * i * voxelDimensionFactors[0] + 
+              rotationMatrix[0][1] * voxelDimensions[1] * j * voxelDimensionFactors[1] + 
+              rotationMatrix[0][2] * voxelDimensions[2] * k * voxelDimensionFactors[2] + 
+              offsets[0];
+              
+      var j = rotationMatrix[1][0] * voxelDimensions[0] * i * voxelDimensionFactors[0] + 
+              rotationMatrix[1][1] * voxelDimensions[1] * j * voxelDimensionFactors[1] + 
+              rotationMatrix[1][2] * voxelDimensions[2] * k * voxelDimensionFactors[2] + 
+              offsets[1];
+              
+      var k = rotationMatrix[2][0] * voxelDimensions[0] * i * voxelDimensionFactors[0] + 
+              rotationMatrix[2][1] * voxelDimensions[1] * j * voxelDimensionFactors[1] + 
+              rotationMatrix[2][2] * voxelDimensions[2] * k * voxelDimensionFactors[2] + 
+              offsets[2];
+              
+      return { x: x, y: y, z: z };
+      */
+    }
     
     console.log( data );
-    
+    console.log( metadata );
+    console.log( voxelToWorld(0, 0, 0) );
+    //console.log( method2Transformation(84.5, 81.156494140625, 155.75299072265625) );
     
   }
   
@@ -105,6 +203,11 @@ class NiftiDecoderAlt extends Filter {
   * @param {Object} header - nifti header
   */
   _scaleData( data, header ){
+    // We dont scale in the case RGB24
+    if( header.datatypeCode == nifti.NIFTI1.TYPE_RGB24 ){
+      return;
+    }
+    
     // the data scaling wont change anything, thus we dont perform it
     if( header.scl_slope == 1 && header.scl_inter == 0 ){
       return;
@@ -260,6 +363,10 @@ class NiftiDecoderAlt extends Filter {
     return typedData;
   }
   
+  
+  _computeSubjsctBasedCoord( header ){
+    
+  }
   
 } /* END of class NiftiDecoderAlt */
 
